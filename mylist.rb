d227abfde508d14ae69126b037a4d587c5434601
jorgegoco <jorgegoco70@gmail.com>
2023-02-02 18:45:25 +0100

    Update the Readme template file

--- mylist.rb
+++ mylist.rb
@@ -11,10 +11,14 @@ include MyEnumerable
     @list.each { |i| yield i }
  end
 end
+
+
  list = MyList.new(1, 2, 3, 4)
+
  list.all? {|e| e < 5}
  list.all? {|e| e > 5}
- list.all? {|e| e <= 2}
+
  list.any? {|e| e == 2}
  list.any? {|e| e == 5}
+ 
  list.filter {|e| e.even?}
